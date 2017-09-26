bb69a1fadcec04f1d3dee782aa017a7f26ba2216
Evgenyg <evgenyg@live.com>
2017-09-26 14:30:04 -0600

    Implemented temp wprking knex solution

--- add_person.js
+++ add_person.js
@@ -1,22 +1,28 @@
-const pg = require("pg");
 const settings = require("./settings"); // settings.json
 const people = process.argv.slice(2);
+var knex = require('knex')({
+    client: 'pg',
+    connection: {
+        user: 'development',
+        password: 'development'
 
-const client = new pg.Client({
-    user: settings.user,
-    password: settings.password,
-    database: settings.database,
-    host: settings.hostname,
-    port: settings.port,
-    ssl: settings.ssl
+    }
 });
-client.connect((err) => {
+
+knex.select('*').from('famous_people')
+    .asCallback(function(err, rows) {
+        if (err) return console.error(err);
+        console.log(rows);
+    });
+
+
+/*client.clientonnect((err) => {
     if (err) {
         return console.error("Connection Error", err);
     }
     readName();
-});
-//Function to connect to database and find query
+});*/
+/*//Function to connect to database and find query
 function readName(result) {
     client.query("SELECT * from famous_people WHERE last_name =  $1::text", people, (err, result) => {
         if (err) {
@@ -32,4 +38,4 @@ function printName(result) {
     for (let person in result.rows) {
         console.log('-' + (parseInt(person) + 1) + ': ' + result.rows[person].first_name + ' ' + result.rows[person].last_name + ', ' + result.rows[person].birthdate.toISOString().substr(0, 10));
     }
-}
+}*/
