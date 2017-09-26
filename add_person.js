076a577bd760ab28e3fa27555a811784bcabba5b
Evgenyg <evgenyg@live.com>
2017-09-26 15:40:50 -0600

    Working knex functions that create/retrieve table

--- add_person.js
+++ add_person.js
@@ -1,5 +1,4 @@
 const settings = require("./settings"); // settings.json
-const people = process.argv.slice(2);
 var knex = require('knex')({
     client: 'pg',
     connection: {
@@ -8,12 +7,35 @@ var knex = require('knex')({
 
     }
 });
+//Creates a table with given parameters from command line. Returns error if input is empty
+function createTable() {
+    if (!process.argv[2] || !process.argv[2] || !process.argv[2]) {
+        console.log("Please input values to insert in the form of: name, last name, birthdate.");
+        knex.destroy();
+    } else {
+        var insert1 = {
+            first_name: process.argv[2],
+            last_name: process.argv[3],
+            birthdate: process.argv[4]
+        }
 
-knex.select('*').from('famous_people')
-    .asCallback(function(err, rows) {
-        if (err) return console.error(err);
-        console.log(rows);
-    });
+        knex.insert(insert1).into('famous_people').then(function(id) {
+            getTable();
+            knex.destroy();
+
+        });
+    }
+}
+createTable();
+
+//Selects the created table, in this case 'famous_people' and returns its contents
+function getTable() {
+    knex.select('*').from('famous_people')
+        .asCallback(function(err, rows) {
+            if (err) return console.error(err);
+            console.log(rows);
+        });
+}
 
 
 /*client.clientonnect((err) => {
