5be3dbbdbb8561e4239aed275621366b090ef11e
Evgenyg <evgenyg@live.com>
2017-09-26 18:21:35 -0600

    Created new migration file  to handle famous_person_id as well as incrament ID

---
+++ migrations/20170927001028_foreignkey.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("public_milestones", (table) => {
+        table.increments();
+        table.integer('famous_person_id');
+        table.string('Description');
+        table.date('Date Achived');
+    })
+};
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('public_milestones');
+};
